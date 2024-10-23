84b2083223954e75011688a440da60a214d0ff9f
Silvan H. <108238800+tk373@users.noreply.github.com>
2024-10-23 16:46:41 +0200

    Remove API Keys

--- React_Trails_App-master/client/src/Auth/fire.js
+++ React_Trails_App-master/client/src/Auth/fire.js
@@ -9,13 +9,7 @@ import { GoogleAuthProvider, getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "",
-  authDomain: "m248-projekt.firebaseapp.com",
-  projectId: "m248-projekt",
-  storageBucket: "m248-projekt.appspot.com",
-  messagingSenderId: "234538111854",
-  appId: "1:234538111854:web:bcc854a1c77f323bfca77f",
-  measurementId: "G-NSH5N7MTMR"
+ //add your config here
 };
 
 // Initialize Firebase
